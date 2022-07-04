a36570cbc6c921899207ef58358e0fc2575717ab
imLIVI <lesly120898@gmail.com>
2022-07-04 11:54:33 +0300

    Implement QuickSort.java

--- Sorting/src/Main.java
+++ Sorting/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
-    public static final int ARRAY_SIZE = 10;
+    public static final int ARRAY_SIZE = 100;
 
     public static void main(String[] args) {
 
@@ -24,17 +24,30 @@ public class Main {
         sortingByChoice.sort();
 
         // Sorting by insertion
+        /**
+         * TODO: Modify because this algorithms works incorrect*/
         System.out.println("\n---Sorting by insertion---");
-        Sorting sortingByInsertion = new SortingByChoice(array);
+        Sorting sortingByInsertion = new SortingByInsertion(array);
         sortingByInsertion.sort();
 
-        System.out.println("\n---Sorting by merge---");
+        // Quick sort
+        /**
+         * TODO: Define complexity and invariant*/
+        System.out.println("\n---Quick sort---");
         int[] arr = Arrays.copyOf(array, ARRAY_SIZE);
-
+        //int[] arr = {8,2,9,4,7,6,1,5,3};
         long startTime = System.nanoTime();
-        MergeSorting.mergeSorting(arr, ARRAY_SIZE);
+        QuickSort.quickSort(arr, 0, ARRAY_SIZE - 1);
         long stopTime = System.nanoTime();
+        System.out.println("Time: " + (stopTime - startTime) + " ns");
+        printArray(arr);
 
+        // Sorting by merge
+        System.out.println("\n---Sorting by merge---");
+        arr = Arrays.copyOf(array, ARRAY_SIZE);
+        startTime = System.nanoTime();
+        MergeSorting.mergeSorting(arr, ARRAY_SIZE);
+        stopTime = System.nanoTime();
         System.out.println("Time: " + (stopTime - startTime) + " ns");
         printArray(arr);
 
--- Sorting/src/QuickSort.java
+++ Sorting/src/QuickSort.java
@@ -1,2 +1,58 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class QuickSort {
+    /**
+     * Program invariant: arr[pivot] - sorted
+     *
+     * Program complexity:
+     * ---- SIZE OF ARRAY after each recursion call ----
+     * 1-st level of recursion:                        (0...n)                                   sum = n
+     * /            \
+     * 2-nd level of recursion:            (0...n/2)              (n/2...n)                      sum = n
+     * /    \               /          \
+     * 3-nd level of recursion:    (0...n/4)    (n/4...n/2)  (n/2...3n/4)    (3n/4...n)          sum = n
+     * ...
+     * On each level the complexity is const = O(n)
+     *
+     * Number of levels = log2(n)
+     *
+     * => So program complexity = O (n*log2(n))
+     *
+     * And we don`t use additional memory (as mergeSort for function "merge")
+     */
+
+    public static void quickSort(int arr[], int lo, int hi) {
+        if (lo < hi) {
+            int pivot = pivoting(arr, lo, hi);
+
+            quickSort(arr, lo, pivot - 1);
+            quickSort(arr, pivot + 1, hi);
+        }
+    }
+
+    public static int pivoting(int[] arr, int lo, int hi) {
+        int pivot = hi;
+        while (lo < hi) {
+            while (arr[lo] <= arr[pivot] && lo < hi) {
+                lo++;
+            }
+            while (arr[hi] >= arr[pivot] && lo < hi) {
+                hi--;
+            }
+            if (lo < hi) {
+                swap(arr, lo, hi);
+            }
+            if (lo == hi) {
+                swap(arr, lo, pivot);
+            }
+        }
+        return lo;
+    }
+
+    public static void swap(int arr[], int i, int j) {
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
 }
