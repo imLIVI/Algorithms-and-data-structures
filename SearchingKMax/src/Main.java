d3d3be94aa66e92a2cef73b4cc2ee9b625f0dc49
imLIVI <lesly120898@gmail.com>
2022-07-08 15:03:59 +0300

    Created better implementation of algorithm Searching for k-maxima

--- SearchingKMax/src/Main.java
+++ SearchingKMax/src/Main.java
@@ -19,6 +19,13 @@ public class Main {
         long stopTime = System.nanoTime();
         System.out.println("Time: " + (stopTime - startTime) + " ns");
 
+        // Better algorithm
+        System.out.println("\n-------- Better implementation --------");
+        startTime = System.nanoTime();
+        betterImplementation(Arrays.copyOf(maxs, NUM_OF_K), Arrays.copyOf(array, ARRAY_SIZE));
+        stopTime = System.nanoTime();
+        System.out.println("Time: " + (stopTime - startTime) + " ns");
+
     }
 
     /**
@@ -60,6 +67,51 @@ public class Main {
         System.out.print("Max elements: ");
         for (int i = 0; i < NUM_OF_K; i++) {
             System.out.print(array[maxs[i]] + " ");
+        }
+        System.out.println();
+    }
+
+    /**
+     * Algorithm:
+     * 1. Go through the array and write to maxs first k elements from array
+     * For example:
+     * array = { 8, 15, 25, 1, 3, 6, 9, 8}
+     * maxs = {8, 15, 25 , 1} when k = 4
+     * 2. Looking for in maxs min value
+     * min = 1
+     * 3. If the following (after the k-position) elements have a value greater than the min, change the min to an element with a large value
+     * maxs = {8, 15, 25 , 1}
+     * min = 1
+     * nextValue = 3 => 3 > 1 => maxs = {8, 15, 25 , 3}
+     *
+     * Repeat 3. while the array runs out
+     *
+     * Complexity:
+     * - external cycle:               n (iterations)
+     * - internal cycle:               k (iterations)
+     *
+     * So: O(n*k)
+     */
+    public static void betterImplementation(int[] maxs, int[] array) {
+        int counterMax = 0;
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            if(counterMax < NUM_OF_K){
+                maxs[counterMax++] = array[i];
+            } else {
+                int min = 0;
+                for (int j = 0; j < NUM_OF_K; j++) {
+                    if (maxs[j] < maxs[min]) {
+                        min = j;
+                    }
+                }
+                if (array[i] > maxs[min]){
+                    maxs[min] = array[i];
+                }
+            }
+        }
+        System.out.print("Max elements: ");
+        for (int i = 0; i < NUM_OF_K; i++) {
+            System.out.print(maxs[i] + " ");
         }
         System.out.println();
     }
